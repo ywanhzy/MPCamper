ff5eb02cf45f2f5dc6da93d372688177f9379dc8
zhoumi1324 <136524845@qq.com>
2018-04-04 15:47:43 +0800

    11111

---
+++ pages/aboutUs/index.js
@@ -0,0 +1,28 @@
+// pages/aboutUs/aboutUs.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      version :'1.1.0'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  followUs: function (){
+    console.log('关注快乐车行公众号');
+  }
+})
--- pages/setting/index.js
+++ pages/setting/index.js
@@ -20,57 +20,27 @@ Page({
                   {
                           icon: '../../images/tel.png',
                           text: '关于我们',
-                          path: '/pages/setting/index'
+                          path: '/pages/aboutUs/index'
                   }
           ]
+  },
+
+  navigateTo: function (e) {
+    const index = e.currentTarget.dataset.index
+    const path = e.currentTarget.dataset.path
+    // console.error(index + path)
+    wx.navigateTo({
+      url: path,
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
   
   },
 
